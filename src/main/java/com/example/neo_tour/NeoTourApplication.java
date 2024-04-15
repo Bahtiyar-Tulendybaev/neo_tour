40d39048b224a718807a1ae4589c1e1dea5e83af
Bahtiyar-Tulendybaev <119873603+Bahtiyar-Tulendybaev@users.noreply.github.com>
2024-04-15 23:34:03 +0600

    initial commit

---
+++ src/main/java/com/example/neo_tour/NeoTourApplication.java
@@ -0,0 +1,13 @@
+package com.example.neo_tour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoTourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoTourApplication.class, args);
+    }
+
+}
